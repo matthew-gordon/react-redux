69239b9cd3e94f6a84ebbedf2a9b8c98ba0ff9c2
Matthew Gordon <laxredemption5525@gmail.com>
2017-06-23 15:31:03 -0600

    Remove logger until real components in

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// import logger from 'redux-logger';
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
