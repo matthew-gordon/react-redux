57e31878e299bf59d5ff50ee49b56e1b5f3badb2
Matthew Gordon <laxredemption5525@gmail.com>
2017-06-23 14:34:40 -0600

    Add hello world skeleton

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const debug = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: debug ? 'inline-sourcemap' : false,
+  entry: path.join(__dirname, '/src/js/index.js'),
+  module: {
+    loaders: [{
+      test:/\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-2']
+      }
+    }]
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './src'
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+}
